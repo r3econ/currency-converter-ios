cfabeacedb20f5ba5975312193a2a16412c46490
r3econ <rafal.sroka.it@gmail.com>
2018-06-27 18:23:31 +0200

    Create simple UI, add table view data source methods

--- CurrencyConverter/MainViewController.swift
+++ CurrencyConverter/MainViewController.swift
@@ -1,25 +1,33 @@
 //
-//  ViewController.swift
-//  CurrencyConverter
-//
 //  Created by Rafał on 27.06.18.
 //  Copyright © 2018 None. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - UITableViewControllerDataSource
+    
+    @IBOutlet var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue and configure the cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyPriceCell")!
+        
+        cell.textLabel!.text = "100.0"
+        
+        return cell
+    }
 
 }
 
